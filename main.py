19e68a29e75567c94f48ef6930ac2f469e0d58a8
DLunomos <dludequeso@gmail.com>
2020-04-22 13:07:42 -0500

    Functioning code

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
 from pybricks.parameters import Port, Button, Color, ImageFile, SoundFile
 from pybricks.tools import wait
 
+# Initialize the large motor. It will be used to drop off our bricks
+motor = Motor(Port.)
+
 # The colored objects are either red, green, blue, or yellow.
 POSSIBLE_COLORS = (Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW)
 
 # Initialize the Color Sensor. It is used to detect the color of the objects.
 color_sensor = ColorSensor(Port.S3)
 brick.display.clear()
+
+# Checks if the sensor and motor are connected to the robot
+assert color_sensor
+assert motor
+speaker.beep(frequency = 670, duration = 100)
+
 # This is the main loop. It waits for you to scan and insert 8 colored objects.
 # Then it sorts them by color. Then the process starts over and you can scan
 # and insert the next set of colored objects.
@@ -19,15 +28,15 @@ while True:
     # Store the color measured by the Color Sensor.
     color = color_sensor.color()
     if color in POSSIBLE_COLORS:
-        brick.display.clear()
         if color == Color.RED:
-            brick.display.text("color is red",(25,50))
+            run_time(500, 1000, then = Stop.HOLD, wait = True)
         if color == Color.GREEN:
-            brick.display.text("color is green",(25,50))
+            run_time(500, 1000, then = Stop.HOLD, wait = True)
+        # Doesn't sense yellow well    
         if color == Color.YELLOW:
-            brick.display.text("color is yellow",(25,50))
+            run_time(500, 1000, then = Stop.HOLD, wait = True)
         if color == Color.BLUE:
-            brick.display.text("color is blue",(25,50))
+            run_time(500, 1000, then = Stop.HOLD, wait = True)
     # Determine if the color read is among POSSIBLE_COLORS
     if color in POSSIBLE_COLORS:
         # if yes, beep the brick
