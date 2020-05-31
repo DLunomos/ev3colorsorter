068b85599fe1bf2167d05eaa486b95ff67c4307a
DLunomos <dludequeso@gmail.com>
2020-05-30 23:16:05 -0500

    added knn calibration and refactoring

--- color_sensor_calibration/knn.py
+++ color_sensor_calibration/knn.py
@@ -1,8 +1,11 @@
 from math import sqrt
+from collections import Counter
 import csv
+import random
+import os
 
 def find_distance(a, b):
-    return sqrt(abs(a[0]-b[0])**2 + abs(a[1]-b[1])**2 + abs(a[2]-b[2]))
+    return sqrt(abs(a[0]-b[0])**2 + abs(a[1]-b[1])**2 + abs(a[2]-b[2])**(.5))
 
 color_list = []
 
@@ -14,9 +17,45 @@ with open("color_list.csv", "r") as color_list_file:
 
         for x in range(3):
             line[x] = int(line[x])
-        line[3] = line[3].replace("\n", "")
+        line[3] = line[3].strip()
         color_list.append(line)
 
+data = color_list.copy()
 
-        
-print(color_list)
+test_set = []
+
+for x in range(int(len(color_list) // (10/7))):
+    random_choice = random.choice(color_list)
+    test_set.append(random_choice)
+    color_list.remove(random_choice)
+
+distance_list = []
+min_distance = []
+
+def knn(color, set, k):
+    distance_list = []
+    min_distance = []
+    min_distance_colors = []
+    distance_colors = []
+    for i in set:
+        distance_colors.append(i[3])
+        distance_list.append(find_distance(color, i))
+    for a in range(k):
+        min_distance_colors.append(distance_colors[distance_list.index(min(distance_list))])
+        distance_list.remove(min(distance_list))
+        distance_colors.pop(distance_list.index(min(distance_list)))
+    c = Counter(min_distance_colors)
+    return c.most_common(1)[0][0]
+
+code = ""
+
+with open(os.path.join("..", "main.py"), "r") as main:
+    code = main.readlines()
+
+code = code[:181]
+
+with open(os.path.join("..", "main.py"), "w") as main:
+    main.writelines(code)
+
+with open(os.path.join("..", "main.py"), "a") as main:
+    main.write("\n" + "data = " + str(data))
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
 from pybricks.parameters import Port, Button, Color, ImageFile, SoundFile
 from pybricks.tools import wait
 
-# The colored objects are either red, green, blue, or yellow.
-POSSIBLE_COLORS = (Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW)
+from collections import Counter
+from math import sqrt
 
 # Initialize the Color Sensor. It is used to detect the color of the objects.
 color_sensor = ColorSensor(Port.S3)
@@ -29,6 +29,24 @@ assert belt_motor
 speaker.beep(frequency = 500, duration = 100)
 assert feed_motor
 speaker.beep(frequency = 670, duration = 100)
+
+def find_distance(a, b):
+    return sqrt(abs(a[0]-b[0])**2 + abs(a[1]-b[1])**2 + abs(a[2]-b[2]))
+
+def knn(color, set, k):
+    distance_list = []
+    min_distance = []
+    min_distance_colors = []
+    distance_colors = []
+    for i in set:
+        distance_colors.append(i[3])
+        distance_list.append(find_distance(color, i))
+    for a in range(k):
+        min_distance_colors.append(distance_colors[distance_list.index(min(distance_list))])
+        distance_list.remove(min(distance_list))
+        distance_colors.pop(distance_list.index(min(distance_list)))
+    c = Counter(min_distance_colors)
+    return c.most_common(1)[0][0]
 
 # This is the main loop. It waits for you to scan and insert 8 colored objects.
 # Then it sorts them by color. Then the process starts over and you can scan
@@ -70,23 +88,18 @@ while True:
         # Show how many colored objects we have already scanned.
         brick.display.text(len(color_list))
 
-        # Wait for the center button to be pressed or a color to be scanned.
-        while True:
-            # Store True if the center button is pressed or False if not.
-            pressed = Button.CENTER in brick.buttons()
-            # Store the color measured by the Color Sensor.
-            color = color_sensor.color()
-            # If the center button is pressed or a color is detected,
-            # break out of the loop.
-            if pressed or color in POSSIBLE_COLORS:
-                break
-
+        # Store True if the center button is pressed or False if not.
+        pressed = Button.CENTER in brick.buttons()
+        # Store the color measured by the Color Sensor.
+        color = color_sensor.color()
+        # If the center button is pressed or a color is detected,
+        # move on.
         if pressed:
             # If the button was pressed, end the loop early.
             # We will no longer wait for any remaining objects
             # to be scanned and added to the chute.
             break
-        else:
+        elif color:
             # Otherwise, a color was scanned.
             # So we add (append) it to the list.
             brick.sound.beep(1000, 100, 100)
@@ -95,9 +108,8 @@ while True:
             # We don't want to register the same color once more if we're
             # still looking at the same object. So before we continue, we
             # wait until the sensor no longer sees the object.
-            while color_sensor.color() in POSSIBLE_COLORS:
-                pass
-            brick.sound.beep(2000, 100, 100)
+            if color_sensor.color() == None:
+                brick.sound.beep(2000, 100, 100)
 
             # Show an arrow pointing to the center button,
             # to ask if we are done.
@@ -114,28 +126,55 @@ while True:
         wait(1000)
 
         # Run the conveyor belt motor to the right position based on the color.
-        if color == Color.BLUE:
+        # This uses a classification technique known as k-nearest neighbors.
+        if knn(color, data, 3) = "BLUE":
             brick.display.clear()
             brick.sound.file(SoundFile.BLUE)
             brick.display.text("color is blue",(25,50))
             belt_motor.run_target(500, 10)
-        elif color == Color.GREEN:
+        elif knn(color, data, 3) = "GREEN":
             brick.display.clear()
-            brick.sound.file(SoundFile.GREEN)
+            brick.sound.file(SoundFile.BLUE)
             brick.display.text("color is green",(25,50))
             belt_motor.run_target(500, 132)
-        elif color == Color.YELLOW:
+        elif knn(color, data, 3) = "YELLOW":
             brick.display.clear()
             brick.sound.file(SoundFile.YELLOW)
             brick.display.text("color is yellow",(25,50))
             belt_motor.run_target(500, 360)
-        elif color == Color.RED:
+        elif knn(color, data, 3) = "RED":
             brick.display.clear()
             brick.sound.file(SoundFile.RED)
             brick.display.text("color is red",(25,50))
             belt_motor.run_target(500, 530)
 
+
+        # Basic Code:
+
+        # if color == Color.BLUE:
+        #     brick.display.clear()
+        #     brick.sound.file(SoundFile.BLUE)
+        #     brick.display.text("color is blue",(25,50))
+        #     belt_motor.run_target(500, 10)
+        # elif color == Color.GREEN:
+        #     brick.display.clear()
+        #     brick.sound.file(SoundFile.GREEN)
+        #     brick.display.text("color is green",(25,50))
+        #     belt_motor.run_target(500, 132)
+        # elif color == Color.YELLOW:
+        #     brick.display.clear()
+        #     brick.sound.file(SoundFile.YELLOW)
+        #     brick.display.text("color is yellow",(25,50))
+        #     belt_motor.run_target(500, 360)
+        # elif color == Color.RED:
+        #     brick.display.clear()
+        #     brick.sound.file(SoundFile.RED)
+        #     brick.display.text("color is red",(25,50))
+        #     belt_motor.run_target(500, 530)
+
         # Now that the conveyor belt is in the correct position,
         # eject the colored object.
         feed_motor.run_angle(1500, 90)
         feed_motor.run_angle(1500, -90)
+
+data = [[10, 60, 20, 'BLUE'], [19, 70, 10, 'BLUE'], [17, 21, 0, 'RED'], [70, 84, 7, 'YELLOW'], [69, 80, 10, 'YELLOW'], [1, 1, 1, 'GREEN']]
